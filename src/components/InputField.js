997d682d537a8e30e8933334addcc6438c9bd45c
noman5945 <abdullahnoman.5945@gmail.com>
2024-04-10 17:21:13 +0600

    Menu Section start

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-const InputField = ({ title, customstyles, width, onChangeFn }) => {
+const InputField = ({
+  title,
+  customstyles,
+  width,
+  onChangeFn,
+  placeholder,
+}) => {
   return (
     <div
-      className={` flex flex-col items-start w-[${
+      className={` flex flex-col items-start my-2 w-[${
         width ? width : 400
       }px] ${customstyles}`}
     >
       <label className=" ms-2">{title ? title : "Pass a title"}</label>
       <input
-        placeholder={title ? title : "Pass a title"}
+        placeholder={placeholder ? placeholder : title}
         required
         className=" border-2 bg-slate-50 rounded-lg w-full h-[35px] p-3 focus:outline-none focus:ring focus:border-blue-500"
         onChange={(e) => onChangeFn(e.target.value)}
--- src/pages/GeneralDetails.js
+++ src/pages/GeneralDetails.js
@@ -7,6 +7,10 @@ const GeneralDetails = () => {
   const [startTime, setStartTime] = useState("");
   const [closeTime, setCloseTime] = useState("");
   const [location, setLocation] = useState("");
+  const [lowestPrice, setLowestPrice] = useState("");
+  const [highestPrice, setHighestPrice] = useState("");
+  const [rating, setRating] = useState("");
+
   const handlePost = (event) => {
     event.preventDefault();
     const resturauntInfo = {
@@ -14,31 +18,63 @@ const GeneralDetails = () => {
       location: location,
       opening: startTime,
       closing: closeTime,
+      lowestPrice: lowestPrice,
+      highestPrice: highestPrice,
+      ratings: rating,
     };
 
     console.log(resturauntInfo);
+    event.target.reset();
   };
   return (
-    <div className=" my-5 flex flex-col px-2">
+    <div className=" my-5 flex flex-col px-2 gap-2">
       <form onSubmit={(e) => handlePost(e)}>
         <InputField
-          title={"Resturent Name"}
+          title={"Restaurant Name"}
+          placeholder={"e.g Kudos"}
           width={600}
           onChangeFn={setRestName}
         />
-        <InputField title={"Location"} width={600} onChangeFn={setLocation} />
+        <InputField
+          title={"Location"}
+          width={600}
+          onChangeFn={setLocation}
+          placeholder={"e.g Mohammadpur"}
+        />
         <div className=" flex flex-row gap-3">
           <InputField
             title={"Open time"}
+            placeholder={"e.g 10 AM"}
             width={10}
             onChangeFn={setStartTime}
           />
           <InputField
             title={"Closing time"}
+            placeholder={"e.g 11 PM"}
             width={10}
             onChangeFn={setCloseTime}
           />
         </div>
+        <div className=" flex flex-row gap-3">
+          <InputField
+            title={"Lowest food price"}
+            placeholder={"e.g 400 Tk"}
+            width={10}
+            onChangeFn={setLowestPrice}
+          />
+          <InputField
+            title={"Highest food price"}
+            placeholder={"e.g 1200 Tk"}
+            width={10}
+            onChangeFn={setHighestPrice}
+          />
+        </div>
+        <InputField
+          title={"Current Rating"}
+          placeholder={"e.g 4.5"}
+          width={10}
+          onChangeFn={setRating}
+        />
         <CustomButton
           title={"Add Resturaunt"}
           type={"submit"}
