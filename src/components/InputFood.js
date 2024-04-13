c3f2e097dcd53e468eebfd278a8e1f46631b7db4
noman5945 <abdullahnoman.5945@gmail.com>
2024-04-13 20:24:24 +0600

    Food menu item section  ongoing

--- src/components/InputFood.js
+++ src/components/InputFood.js
@@ -1,7 +1,31 @@
 import React from "react";
+import InputField from "./InputField";
 
-const InputFood = () => {
-  return <div>Input food</div>;
+const InputFood = ({ nameFn, typeFn, priceFn }) => {
+  return (
+    <div className=" flex flex-col bg-platinum rounded-lg">
+      <div className=" grid grid-cols-1 lg:grid-cols-3 gap-2 p-4">
+        <InputField
+          title={"Food Name"}
+          width={15}
+          placeholder={"e.g Burger"}
+          onChangeFn={nameFn}
+        />
+        <InputField
+          title={"Type"}
+          width={15}
+          placeholder={"e.g Fastfood"}
+          onChangeFn={typeFn}
+        />
+        <InputField
+          title={"Price(Tk)"}
+          width={15}
+          placeholder={"e.g 500"}
+          onChangeFn={priceFn}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default InputFood;
--- src/pages/FoodMenu.js
+++ src/pages/FoodMenu.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import InputFood from "../components/InputFood";
+import CustomButton from "../components/CustomButton";
 
 const FoodMenu = () => {
+  const [foodName, setFoodName] = useState("");
+  const [foodType, setFoodtype] = useState("");
+  const [foodprice, setFoodPrice] = useState("");
+  const [foodMenu, setFoodMenu] = useState([]);
+
+  const addToMenu = () => {
+    const foodItem = {
+      foodName: foodName,
+      foodType: foodType,
+      foodPrice: foodprice,
+    };
+    setFoodMenu([...foodMenu, foodItem]);
+    console.log(foodMenu);
+  };
+
   return (
     <div className=" flex flex-col">
       <p className=" font-bold text-xl">Food Menu</p>
-      <div>
-        <InputFood />
+      <div id="food-item-list" className="input-food">
+        <div id="food-item">
+          <InputFood
+            nameFn={setFoodName}
+            typeFn={setFoodtype}
+            priceFn={setFoodPrice}
+          />
+        </div>
       </div>
+      <div className=" lg:w-[50%]">
+        <CustomButton
+          title={"Add New Food Item"}
+          type={"button"}
+          btnFn={addToMenu}
+        />
+      </div>
+
+      {foodMenu.map((item, index) => (
+        <div key={index} className=" flex flex-col ">
+          <div className=" flex flex-row gap-4">
+            <div>{item.foodName}</div>
+            <div>{item.foodType}</div>
+            <div>{item.foodPrice}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
